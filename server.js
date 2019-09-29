852c4093fa870523eff546cccee0923876f2af8c
Josh <romeroj@knights.ucf.edu>
2019-09-29 02:25:45 -0400

    Testing server edit for website

--- server.js
+++ server.js
@@ -41,6 +41,16 @@ app.use("/api/contacts", contact);
 
 const port = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+}
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
 app.listen(port, function() {
     console.log("Server is up and running on Port: " + port);
 });
+
+
