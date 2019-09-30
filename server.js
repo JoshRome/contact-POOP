dc41e59dea762349567529b5cff42e57f536f84f
JoshRome <romeroj1026@gmail.com>
2019-09-29 22:01:06 -0400

    Changing Server.js

--- server.js
+++ server.js
@@ -10,14 +10,14 @@ const contact = require("./routes/api/contacts")
 
 const app = express();
 
-//
-// if (process.env.NODE_ENV === 'production') {
-//   //set static folder
-//   app.use(express.static('client/build'));
-// }
-// // app.get('*', (req, res) => {
-// //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-// // });
+
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+}
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+// });
 
 //bodyParser middleware
 app.use(bodyParser.urlencoded({
